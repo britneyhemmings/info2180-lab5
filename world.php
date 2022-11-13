72991c314cc55c3fe1788fa199fad738b6817358
Britney Hemmings <britneyhemmings1@gmail.com>
2022-11-13 00:54:13 -0500

    Edited starter files-ex4

--- world.php
+++ world.php
@@ -12,7 +12,28 @@ $stmt = $conn->query("SELECT * FROM countries WHERE name LIKE '%$input%'");
 
 $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
-foreach($results as $row){
+/*foreach(){
    echo '<li>' . $row['name'] . ' is ruled by ' . $row['head_of_state'] . '</li>'; 
-}
+}*/
 ?>
+
+<table>
+  <thead>
+    <tr>
+      <th>Country Name</th>
+      <th>Continent</th>
+      <th>Independence Year</th>
+      <th>Head of State</th>
+    </tr>
+  </thead>
+  <tbody>
+  <?php foreach ($results as $row): ?>
+  <tr>
+    <td><?= $row['name']; ?></td>
+    <td><?= $row['continent']; ?></td>
+    <td><?= $row['independence_year']; ?></td>
+    <td><?= $row['head_of_state']; ?></td>
+  </tr>
+ <?php endforeach; ?>
+ </tbody>
+</table>
